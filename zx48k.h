c5fd681bfaa46be1220db99f3f3d3561643d29c6
Witold Krecicki <wpk@isc.org>
2016-07-16 01:33:36 +0200

    commit 9537cb82c08875f9714cc7f5db8e80821bd07eb7 Author: Mateusz Szymański <orhtej2@gmail.com> Date:   Fri Jul 15 22:42:23 2016 +0200
        Fixed compilation warnings.

--- zx48k.h
+++ zx48k.h
@@ -9,6 +9,7 @@
 #define ZX48K_H_
 #include <set>
 #include <string>
+
 #include "bushandler.h"
 #include "emusdl.h"
 #include "z80.h"
@@ -21,10 +22,10 @@ public:
 	void initialize();
 	void run();
 
-	virtual uint32_t readmem(uint16_t address);
-	virtual void writemem(uint16_t address, uint8_t value);
-	virtual uint8_t readio(uint16_t address);
-	virtual void writeio(uint16_t address, uint8_t value);
+	virtual uint32_t readmem(uint16_t address) override;
+	virtual void writemem(uint16_t address, uint8_t value) override;
+	virtual uint8_t readio(uint16_t address) override;
+	virtual void writeio(uint16_t address, uint8_t value) override;
 
 
 private:
